fe1cea060f32651c55ef4078bf8564e12425450f
Jiayi Ren <jiayiren22@gmail.com>
2020-07-06 15:53:16 -0700

    complete first 10 exercises

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,13 +35,21 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
-
+waypoints.append({"lat": 100, "lon": -200, "name": "new place"})
+print(waypoints)
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
 # YOUR CODE HERE
-
+for i in waypoints:
+    if i["name"] == "a place":
+        i["lon"] = -130
+        i["name"] = "not a real place"
+        break
+print(waypoints)
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for i in waypoints:
+    print(f"lat is {i['lat']}, lon is {i['lon']}, name is {i['name']}")
--- src/10_functions.py
+++ src/10_functions.py
@@ -9,4 +9,9 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+def even_checker(n):
+    if n % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd")
+even_checker(num)
